197a5cfde731c1b66f1f9ae13a0cea824c10ab9e
Fahim Imaduddin Dalvi <faimaduddin@qf.org.qa>
2018-08-08 04:24:27 -0400

    Moved data loading to its own source file

--- train_classifier.py
+++ train_classifier.py
@@ -19,6 +19,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 import aux_classifier.utils as utils
 import aux_classifier.representations as repr
+import aux_classifier.data_loader as data_loader
 
 def main():
     parser = argparse.ArgumentParser(description='Train a classifier')
@@ -73,114 +74,12 @@ def main():
     print("Number of train sentences: %d"%(len(train_activations)))
     print("Number of test sentences: %d"%(len(test_activations)))
 
-    def load_aux_data(source_path, labels_path, source_aux_path, activations):
-        tokens = {
-            'source_aux': [],
-            'source': [],
-            'target': []
-        }
-
-        with open(source_aux_path) as fp:
-            for line in fp:
-                line_tokens = line.strip().split()
-                if len(line_tokens) > args.max_sent_l:
-                    continue
-                tokens['source_aux'].append(line_tokens)
-        with open(source_path) as fp:
-            for line in fp:
-                line_tokens = line.strip().split()
-                if len(line_tokens) > args.max_sent_l:
-                    continue
-                tokens['source'].append(line_tokens)
-
-        with open(labels_path) as fp:
-            for line in fp:
-                line_tokens = line.strip().split()
-                if len(line_tokens) > args.max_sent_l:
-                    continue
-                tokens['target'].append(line_tokens)
-
-        # Check if all data is well formed (whether we have activations + labels for each and every word)
-        invalid_activation_idx = []
-        for idx, activation in enumerate(activations):
-            if activation.shape[0] == len(tokens['source_aux'][idx]) and len(tokens['source'][idx]) == len(tokens['target'][idx]):
-                pass
-            else:
-                invalid_activation_idx.append(idx)
-                print ("Skipping line: ", idx, "A: %d, aux: %d, src: %d, tgt: %s"%(activation.shape[0], len(tokens['source_aux'][idx]), len(tokens['source'][idx]), len(tokens['target'][idx])))
-        
-        for num_deleted, idx in enumerate(invalid_activation_idx):
-            print("Deleting line %d: %d activations, %d source, %d target"%
-                (idx - num_deleted, 
-                    activations[idx - num_deleted].shape[0], 
-                    len(tokens['source'][idx - num_deleted]), 
-                    len(tokens['target'][idx - num_deleted])
-                )
-            )
-            del(activations[idx - num_deleted])
-            del(tokens['source_aux'][idx - num_deleted])
-            del(tokens['source'][idx - num_deleted])
-            del(tokens['target'][idx - num_deleted])
-            
-        for idx, activation in enumerate(activations):
-            assert(activation.shape[0] == len(tokens['source_aux'][idx]))
-            assert(len(tokens['source'][idx]) == len(tokens['target'][idx]))
-        
-        return tokens
-
-    def load_data(source_path, labels_path, activations):
-        tokens = {
-            'source': [],
-            'target': []
-        }
-
-        with open(source_path) as fp:
-            for line in fp:
-                line_tokens = line.strip().split()
-                if len(line_tokens) > args.max_sent_l:
-                    continue
-                tokens['source'].append(line_tokens)
-
-        with open(labels_path) as fp:
-            for line in fp:
-                line_tokens = line.strip().split()
-                if len(line_tokens) > args.max_sent_l:
-                    continue
-                tokens['target'].append(line_tokens)
-
-        # Check if all data is well formed (whether we have activations + labels for each and every word)
-        invalid_activation_idx = []
-        for idx, activation in enumerate(activations):
-            if activation.shape[0] == len(tokens['source'][idx]) and activation.shape[0] == len(tokens['target'][idx]):
-                pass
-            else:
-                invalid_activation_idx.append(idx)
-                print ("Skipping line: ", idx)
-        
-        for num_deleted, idx in enumerate(invalid_activation_idx):
-            print("Deleting line %d: %d activations, %d source, %d target"%
-                (idx - num_deleted, 
-                    activations[idx - num_deleted].shape[0], 
-                    len(tokens['source'][idx - num_deleted]), 
-                    len(tokens['target'][idx - num_deleted])
-                )
-            )
-            del(activations[idx - num_deleted])
-            del(tokens['source'][idx - num_deleted])
-            del(tokens['target'][idx - num_deleted])
-            
-        for idx, activation in enumerate(activations):
-            assert(activation.shape[0] == len(tokens['source'][idx]))
-            assert(activation.shape[0] == len(tokens['target'][idx]))
-        
-        return tokens
-
     if args.exp_type == 'word' or args.exp_type == 'charcnn':
-        train_tokens = load_data(args.train_source, args.train_labels, train_activations)
-        test_tokens = load_data(args.test_source, args.test_labels, test_activations)
+        train_tokens = data_loader.load_data(args.train_source, args.train_labels, train_activations, args.max_sent_l)
+        test_tokens = data_loader.load_data(args.test_source, args.test_labels, test_activations, args.max_sent_l)
     else:
-        train_tokens = load_aux_data(args.train_source, args.train_labels, args.train_aux_source, train_activations)
-        test_tokens = load_aux_data(args.test_source, args.test_labels, args.test_aux_source, test_activations)
+        train_tokens = data_loader.load_aux_data(args.train_source, args.train_labels, args.train_aux_source, train_activations, args.max_sent_l)
+        test_tokens = data_loader.load_aux_data(args.test_source, args.test_labels, args.test_aux_source, test_activations, args.max_sent_l)
 
     NUM_TOKENS = sum([len(t) for t in train_tokens['target']])
     print('Number of total train tokens: %d'%(NUM_TOKENS))
