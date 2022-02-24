098f702f2d4975849246dd35919f4c27c3af0aaa
Hassan Sajjad <hassaan84s@gmail.com>
2022-02-24 10:55:48 +0300

    Added binary annotation, tests and data utils

--- neurox/data/annotate.py
+++ neurox/data/annotate.py
@@ -1,13 +1,14 @@
-"""Given a list of sentence and their activations, create a binary labeled dataset based on a
-regular expression, a list of words and a fuction. 
+"""Given a list of sentences, their activations and a pattern, create a binary labeled dataset based on the pattern
+where pattern can be a regular expression, a list of words and a function. 
 For example, one can create a binary dataset of years vs. not-years (2004 vs. this) by specifying the regular expression
 that matches the pattern of year. 
-The program will extract positive examples based on the provided filter and will randomly extract negative examples
-of the same size as that of the positive examples. The output of the program is a word file, a label file and an activation file.
+The program will extract positive class examples based on the provided filter and will consider rest of the examples as
+negative class examples. The output of the program is a word file, a label file and an activation file.
 
 Author: Hassan Sajjad
 Last Modified: 4 July, 2021
 Last Modified: 21 February, 2022
+Last Modified: 24 February, 2022
 """
 
 import re
@@ -16,14 +17,13 @@ import numpy as np
 import argparse
 import sys
 
-from neurox.data.writer import ActivationsWriter
 import neurox.data.loader as data_loader
+import neurox.data.utils as data_utils
 
-def _create_binary_data(tokens, activations, binary_filter, num_layers, output_file, output_type="autodetect", decompose_layers=False,
-    filter_layers=None,):
+def _create_binary_data(tokens, activations, binary_filter, balance_data=False):
     """
-    Given a set of sentences and per word activations, create a binary dataset using the provided pattern of the
-    positive class. A pattern can be a set of words, a regex object or a function
+    Given a list of tokens, their activations and a binary_filter, create the binary labeled data. A binary filter can be
+    a set, regex or a function.
     
     Parameters
     ----------
@@ -32,16 +32,18 @@ def _create_binary_data(tokens, activations, binary_filter, num_layers, output_f
     activations: list
         A list of sentence-wise activations 
     binary_filter: a set of words or a regex object or a function
-    num_layers: number of layers
     
     Returns
     -------
-    Saves a word file, a binary label file and their activations
+    annotated_dataset : tuple of (tokens, labels and activations)
+        A list of selected positive and negative class words, their labels (the
+        string "positive" for examples matching the filter and "negative" for
+        others) and their activations
     
     Example
     -------
-    create_binary_data(tokens, activations, re.compile(r'^\w\w$'), 12) select words of two characters only as a positive class
-    create_binary_data(tokens, activations, {'is', 'can'}, 12) select occrrences of 'is' and 'can' as a positive class
+    _create_binary_data(tokens, activations, re.compile(r'^\w\w$')) select words of two characters only as a positive class
+    _create_binary_data(tokens, activations, {'is', 'can'}) select occrrences of 'is' and 'can' as a positive class
     """
     
     filter_fn = None
@@ -64,118 +66,59 @@ def _create_binary_data(tokens, activations, binary_filter, num_layers, output_f
         for w_idx, word in enumerate(sentences):
             if filter_fn(word):
                 positive_class_words.append(word)
-                positive_class_activations.append(activations[s_idx][w_idx].reshape((num_layers, 1, -1)))
+                positive_class_activations.append(activations[s_idx][w_idx])
             else:
                 negative_class_words.append(word)
-                negative_class_activations.append(activations[s_idx][w_idx].reshape((num_layers, 1, -1)))
+                negative_class_activations.append(activations[s_idx][w_idx])
 
-    negative_class_words, negative_class_activations = _balance_negative_class(negative_class_words, negative_class_activations, len(positive_class_words))    
+    if len(negative_class_words) == 0 or len(positive_class_words) == 0:
+        raise ValueError(
+            "Positive or Negative class examples are zero"
+        )
+    elif len(negative_class_words) < len(positive_class_words):
+        print ("WARNING: the negative class examples are less than the positive class examples")
+        print ("Postive class examples: ", len(positive_class_words), "Negative class examples: ", len(negative_class_words))
+
+    if balance_data:
+        negative_class_words, negative_class_activations = data_utils._balance_negative_class(negative_class_words, negative_class_activations, len(positive_class_words))
     
     print ("Number of Positive examples: ", len(positive_class_words))
     
-    labels = (['positive']*len(positive_class_words)) + ['negative']*len(positive_class_words)
+    labels = (['positive']*len(positive_class_words)) + ['negative']*len(negative_class_words)
     
-    save_files(positive_class_words+negative_class_words, labels, positive_class_activations+negative_class_activations, output_file, output_type, decompose_layers,
+    return positive_class_words+negative_class_words, labels, positive_class_activations+negative_class_activations
+
+
+
+def annotate_data(source_path, activations_path, binary_filter, output_prefix, output_type="hdf5", decompose_layers=False, filter_layers=None):
+    """
+    Given a set of sentences, per word activations, a binary_filter and output_prefix, creates binary data and save it to the disk. 
+    A binary filter can be a set of words, a regex object or a function
+    
+    Parameters
+    ----------
+    source_path : text file with one sentence per line 
+    activations: list
+        A list of sentence-wise activations 
+    binary_filter: a set of words or a regex object or a function
+    output_prefix: prefix of the output files that will be saved as the output of this script
+    
+    Returns
+    -------
+    Saves a word file, a binary label file and their activations
+    
+    Example
+    -------
+    annotate_data(source_path, activations_path, re.compile(r'^\w\w$')) select words of two characters only as a positive class
+    annotate_data(source_path, activations_path, {'is', 'can'}) select occrrences of 'is' and 'can' as a positive class
+    """
+
+    activations, num_layers = data_loader.load_activations(activations_path)
+
+    # giving source_path instead of labels since labels will be generated later
+    tokens = data_loader.load_data(source_path, source_path, activations, max_sent_l=512)
+
+    words, labels, activations = _create_binary_data(tokens, activations, binary_filter)
+    activations = [np.swapaxes(a.reshape((a.shape[1], num_layers, -1)), 0, 1) for a in activations]
+    data_utils.save_files(words, labels, activations, output_prefix, output_type, decompose_layers,
     filter_layers)
-    
-    
-def _balance_negative_class(words, activations, positive_class_size):
-    """
-    Helper function to shuffle the negative class instances and select the number of instances equal to 
-    the positive class
-    
-    Parameters
-    ----------
-    words: list
-        A list of words
-    activations: list
-        A list of word-wise activations 
-    positive_class_size: number of words to select
-    
-    Returns
-    -------
-    A list of words and activations equal in size to the passed length
-    
-    """    
-    print ("Balancing Negative class ...")
-    indices = list(range((len(words))))
-    np.random.shuffle(indices)
-
-    swords = []
-    sactivations = []
-    for i in range(positive_class_size):
-        swords.append(words[indices[i]])
-        sactivations.append(activations[indices[i]])
-    
-    return swords, sactivations
-    
-def save_files(words, labels, activations, output_file, output_type, decompose_layers, filter_layers):
-    
-    """
-    Save word and label file in text format and activations in hdf5 format
-    
-    Parameters
-    ----------
-    words: list
-        A list of words
-    labels: list
-        A list of labels for every word
-    activations: list
-        A list of word-wise activations 
-    positive_class_size: number of words to select
-    
-    Returns
-    -------
-    Save word, label and activation files
-    
-    """ 
-    print("Preparing output files")
-
-    with open(output_file+".word", "w") as file:
-        print(*words, sep = "\n", file = file)
-        file.close()
-
-    with open(output_file+".label", "w") as file:
-        print(*labels, sep = "\n", file = file)
-        file.close()
-    
-    writer = ActivationsWriter.get_writer(output_file, filetype=output_type, decompose_layers=decompose_layers, filter_layers=filter_layers)
-
-    for word_idx, word in enumerate(words):
-        writer.write_activations(word_idx, [word], activations[word_idx])
-
-    writer.close()
-
-
-def annotate_data(sentences, activations)
-
-    activations, num_layers = data_loader.load_activations(activations, 768)
-    # giving sentences instead of labels since labels will be generated later
-    tokens = data_loader.load_data(sentences, sentences, activations, 512)
-
-    _create_binary_data(tokens, activations, binary_filter, num_layers, output_file, output_type="autodetect", decompose_layers=False,
-    filter_layers=None,)
-
-
-def main():
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input_file", help="Text file path with one sentene per line")
-    parser.add_argument("--activations", help="Activation file")
-    parser.add_argument("--binary_filter", help="A set, pattern or function based on which binary data will be created")
-    parser.add_argument("--out_file", help="Prefix of output file")
-    parser.add_argument("--output_type", help="Type of output activation file, json or hdf5")
-    parser.add_argument("--decompose_layers", help="A boolean value to specify if a layers need to be save separately")
-    parser.add_argument("--filter_layers", help="Specify a layer if activations of only a particular layer needs to be saved")
-
-    annotate_data(arg.input_file,
-                 arg.activations,
-                 arg.binary_filter,
-                 arg.output_file,
-                 arg.output_type,
-                 arg.decompose_layers,
-                 arg.filter_layers)
-    
-
-    if __name__ == "__main__":
-    main()
