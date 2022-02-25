1ba7b08e6f33c5f34071a3dac291c6f17dc27759
davidarps <david-arps@gmx.de>
2022-02-25 15:48:29 +0100

    code formatting

--- neurox/control_tasks/seq_labeling_ct.py
+++ neurox/control_tasks/seq_labeling_ct.py
@@ -3,11 +3,17 @@ from collections import Counter
 import numpy as np
 
 
-def create_seq_labeling_control_task_datasets(train_tokens, sample_from='same', dev_source=None, test_source=None, case_sensitive=True):
+def create_seq_labeling_control_task_datasets(
+    train_tokens,
+    sample_from="same",
+    dev_source=None,
+    test_source=None,
+    case_sensitive=True,
+):
     """
-    Method that prepares labels for a control task, as defined in §2.1 of `Hewitt and Liang (2019) <https://aclanthology.org/D19-1275.pdf>` 
+    Method that prepares labels for a control task, as defined in §2.1 of `Hewitt and Liang (2019) <https://aclanthology.org/D19-1275.pdf>`
 
-    Target classes are selected randomly for each token type in the datasets. 
+    Target classes are selected randomly for each token type in the datasets.
     The number of control task classes is the same as the number of classes in ``train_tokens['target']``. The distribution of control task labels can be specified.
 
     Parameters
@@ -19,34 +25,34 @@ def create_seq_labeling_control_task_datasets(train_tokens, sample_from='same', 
     test_source : list, optional
         List containing the ``source`` tokens from the test set, as produced by ``test_tokens['source']``
     sample_from : str, optional
-        defaults to 'same'. The distribution from which control task labels are sampled. 
+        defaults to 'same'. The distribution from which control task labels are sampled.
         'same': Labels are sampled from the same distribution as the main task labels.
         'uniform': Labels are sampled from a uniform distribution.
     case_sensitive: bool, optional
-        defaults to True. Sets whether the token comparison (for assigning the control task labels) is case-sensitive 
+        defaults to True. Sets whether the token comparison (for assigning the control task labels) is case-sensitive
         or case-insensitive.
 
     Returns
     -------
     ct_tokens : list
-        A list with either one, two or three elements - depending on whether 
+        A list with either one, two or three elements - depending on whether
         control task labels for only the train, or also dev and test set should be created.
         Each element of the list is a dictionary containing two lists, ``source`` and ``target``.
-        The ``source`` list is the same as from the ``tokens`` input. 
+        The ``source`` list is the same as from the ``tokens`` input.
         The ``target`` list is the list of control task labels.
     """
-    
+
     # compute label stats in task training data
-    labels_flat = [l for sublist in train_tokens['target'] for l in sublist]
+    labels_flat = [l for sublist in train_tokens["target"] for l in sublist]
     label_freqs = Counter(labels_flat)
     ct_labels = list(range(len(label_freqs)))
     ct_label_distr = [v / sum(label_freqs.values()) for v in label_freqs.values()]
-    if sample_from == 'uniform':
-        ct_label_distr = [1/len(ct_label_distr) for i in ct_labels]
+    if sample_from == "uniform":
+        ct_label_distr = [1 / len(ct_label_distr) for i in ct_labels]
 
-    # create control task labels 
+    # create control task labels
     word_types_to_ct_label = dict()
-    datasets = [train_tokens['source']]
+    datasets = [train_tokens["source"]]
     if dev_source is not None:
         datasets.append(dev_source)
     if test_source is not None:
@@ -66,8 +72,6 @@ def create_seq_labeling_control_task_datasets(train_tokens, sample_from='same', 
                     word_types_to_ct_label[tok] = label_for_tok
             ct_target.append(ct_labels_for_sent)
         assert len(source_dataset) == len(ct_target)
-        assert all([len(s) == len(t) for s,t in zip(source_dataset, ct_target)])
-        result.append({'source': source_dataset, 'target': ct_target})
+        assert all([len(s) == len(t) for s, t in zip(source_dataset, ct_target)])
+        result.append({"source": source_dataset, "target": ct_target})
     return result
-
-
--- tests/control_tasks/test_seq_labeling_ct.py
+++ tests/control_tasks/test_seq_labeling_ct.py
@@ -8,65 +8,113 @@ import numpy as np
 
 # def create_seq_labeling_control_task_datasets(train_tokens, sample_from='same', dev_source=None, test_source=None):
 
-class TestCreateSeqLabelingControlTaskDatasets(unittest.TestCase):
 
+class TestCreateSeqLabelingControlTaskDatasets(unittest.TestCase):
     def setUp(self):
-        self.three_label_data_skewed = {'source': [['Fly', 'me', 'to', 'the', 'moon'],
-        ['I', 'am', 'walking', 'on', 'the', 'moon'],
-        ['Now', 'I', 'am', 'walking', 'on', 'a', 'dream']],
-        'target': [['A', 'A', 'A', 'B', 'A'],
-        ['A', 'B', 'A', 'A', 'B', 'C'],
-        ['B', 'A', 'B', 'A', 'A', 'B', 'A']]}
-        self.two_labels_dataset_balanced = {'source': [['I', 'am', 'walking', 'on', 'sunshine'],
-        'Tambourines', 'and', 'elephants', 'are', 'playin', 'in', 'the', 'band'],
-        'target': [['B', 'A', 'A', 'B', 'A'],
-        ['B', 'A', 'B', 'A', 'B', 'A', 'B', 'A']]}
-        self.small_dataset_four_labels = {'source': [['Is', 'There', 'life', 'on', 'Mars']], 
-        'target': [['A', 'B', 'C', 'D', 'A']]}
-        
+        self.three_label_data_skewed = {
+            "source": [
+                ["Fly", "me", "to", "the", "moon"],
+                ["I", "am", "walking", "on", "the", "moon"],
+                ["Now", "I", "am", "walking", "on", "a", "dream"],
+            ],
+            "target": [
+                ["A", "A", "A", "B", "A"],
+                ["A", "B", "A", "A", "B", "C"],
+                ["B", "A", "B", "A", "A", "B", "A"],
+            ],
+        }
+        self.two_labels_dataset_balanced = {
+            "source": [
+                ["I", "am", "walking", "on", "sunshine"],
+                "Tambourines",
+                "and",
+                "elephants",
+                "are",
+                "playin",
+                "in",
+                "the",
+                "band",
+            ],
+            "target": [
+                ["B", "A", "A", "B", "A"],
+                ["B", "A", "B", "A", "B", "A", "B", "A"],
+            ],
+        }
+        self.small_dataset_four_labels = {
+            "source": [["Is", "There", "life", "on", "Mars"]],
+            "target": [["A", "B", "C", "D", "A"]],
+        }
+
         ints_list_of_lists = [[str(i)] for i in range(1000)]
-        modulo_seven_target = [['T'] if i % 7 == 0 else ['F'] for i in range(1000) ]
-        self.modulo_seven_dataset = {'source': ints_list_of_lists, 'target': modulo_seven_target}
+        modulo_seven_target = [["T"] if i % 7 == 0 else ["F"] for i in range(1000)]
+        self.modulo_seven_dataset = {
+            "source": ints_list_of_lists,
+            "target": modulo_seven_target,
+        }
 
     def test_same_words_have_same_ct_label_in_same_dataset(self):
-        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(self.three_label_data_skewed)
-        the_moon = ct_tokens['source'][0][3:] == ct_tokens['source'][1][4:] and ct_tokens['target'][0][3:] == ct_tokens['target'][1][4:]
-        eye_am_walking_on = ct_tokens['source'][1][0:4] == ct_tokens['source'][2][1:5] and ct_tokens['target'][1][0:4] == ct_tokens['target'][2][1:5]
+        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(
+            self.three_label_data_skewed
+        )
+        the_moon = (
+            ct_tokens["source"][0][3:] == ct_tokens["source"][1][4:]
+            and ct_tokens["target"][0][3:] == ct_tokens["target"][1][4:]
+        )
+        eye_am_walking_on = (
+            ct_tokens["source"][1][0:4] == ct_tokens["source"][2][1:5]
+            and ct_tokens["target"][1][0:4] == ct_tokens["target"][2][1:5]
+        )
         assert the_moon and eye_am_walking_on
 
     def test_same_words_have_same_ct_label_in_diff_datasets(self):
-        [ct_tokens_train, ct_tokens_dev] = ct.create_seq_labeling_control_task_datasets(self.three_label_data_skewed, dev_source=self.two_labels_dataset_balanced['source'])
-        eye_am_walking_on = ct_tokens_train['source'][1][0:4] == ct_tokens_dev['source'][0][0:4] and ct_tokens_train['target'][1][0:4] == ct_tokens_dev['target'][0][0:4]
+        [ct_tokens_train, ct_tokens_dev] = ct.create_seq_labeling_control_task_datasets(
+            self.three_label_data_skewed,
+            dev_source=self.two_labels_dataset_balanced["source"],
+        )
+        eye_am_walking_on = (
+            ct_tokens_train["source"][1][0:4] == ct_tokens_dev["source"][0][0:4]
+            and ct_tokens_train["target"][1][0:4] == ct_tokens_dev["target"][0][0:4]
+        )
         assert eye_am_walking_on
 
     def test_num_ct_labels_exactly_matches_num_training_labels_two_labels(self):
         """The number of control task labels is smaller or equal to the number of labels in the task training data"""
-        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(self.two_labels_dataset_balanced, sample_from='uniform')
-        labels_flat = [l for sublist in ct_tokens['target'] for l in sublist]
-        assert len(set(labels_flat)) == 2 
+        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(
+            self.two_labels_dataset_balanced, sample_from="uniform"
+        )
+        labels_flat = [l for sublist in ct_tokens["target"] for l in sublist]
+        assert len(set(labels_flat)) == 2
 
     def test_num_ct_labels_roughly_matches_num_training_labels_three_labels(self):
         """The number of control task labels is smaller or equal to the number of labels in the task training data"""
-        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(self.three_label_data_skewed, sample_from='same')
-        labels_flat = [l for sublist in ct_tokens['target'] for l in sublist]
+        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(
+            self.three_label_data_skewed, sample_from="same"
+        )
+        labels_flat = [l for sublist in ct_tokens["target"] for l in sublist]
         assert len(set(labels_flat)) <= 3
 
     def test_num_ct_labels_roughly_matches_num_training_labels_four_labels(self):
         """The number of control task labels is smaller or equal to the number of labels in the task training data"""
-        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(self.small_dataset_four_labels, sample_from='same')
-        labels_flat = [l for sublist in ct_tokens['target'] for l in sublist]
+        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(
+            self.small_dataset_four_labels, sample_from="same"
+        )
+        labels_flat = [l for sublist in ct_tokens["target"] for l in sublist]
         assert len(set(labels_flat)) <= 4
 
     def test_uniform_distribution_matches(self):
         """Regardless of the label distribution in the training data, the control task labels are in a more or less uniform distribution"""
-        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(self.modulo_seven_dataset, sample_from='uniform')
-        labels_flat = [l for sublist in ct_tokens['target'] for l in sublist]
+        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(
+            self.modulo_seven_dataset, sample_from="uniform"
+        )
+        labels_flat = [l for sublist in ct_tokens["target"] for l in sublist]
         label_freqs_dict = Counter(labels_flat)
         assert max(label_freqs_dict.values()) < 600
 
     def test_same_distribution_matches(self):
         """The label distribution in the control task data roughly matches the label distribution in the training data"""
-        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(self.modulo_seven_dataset, sample_from='same')
-        labels_flat = [l for sublist in ct_tokens['target'] for l in sublist]
+        [ct_tokens] = ct.create_seq_labeling_control_task_datasets(
+            self.modulo_seven_dataset, sample_from="same"
+        )
+        labels_flat = [l for sublist in ct_tokens["target"] for l in sublist]
         label_freqs_dict = Counter(labels_flat)
         assert min(label_freqs_dict.values()) < 200
