9fcca137e12d924eb4a1745e7a07e66308ec9f68
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2022-03-27 17:18:59 +0300

    Added scripts for code formatting

--- scripts/format_code.sh
+++ scripts/format_code.sh
@@ -5,8 +5,9 @@ set -e
 
 if [[ ! -f setup.cfg ]] || [[ ! -d neurox ]] || [[ ! -d scripts ]] || [[ ! -d tests ]]
 then
-  echo "run_tests.sh must be run from the root of the repository"
+  echo "format_code.sh must be run from the root of the repository"
   exit 1
 fi
 
-python -m pytest -vv --cov
+ufmt format neurox
+ufmt format tests
