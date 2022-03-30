4df3f6ff795724423800327882163fec9e7ef3c2
davidarps <david-arps@gmx.de>
2022-03-30 13:33:46 +0200

    adapt asserts in tests

--- tests/data/test_control_task.py
+++ tests/data/test_control_task.py
@@ -53,34 +53,26 @@ class TestCreateSeqLabelingControlTaskDatasets(unittest.TestCase):
         [ct_tokens] = ct.create_sequence_labeling_dataset(
             self.three_label_data_skewed
         )
-        the_moon = (
-            ct_tokens["source"][0][3:] == ct_tokens["source"][1][4:]
-            and ct_tokens["target"][0][3:] == ct_tokens["target"][1][4:]
-        )
-        eye_am_walking_on = (
-            ct_tokens["source"][1][0:4] == ct_tokens["source"][2][1:5]
-            and ct_tokens["target"][1][0:4] == ct_tokens["target"][2][1:5]
-        )
-        assert the_moon and eye_am_walking_on
+        self.assertEqual(ct_tokens["source"][0][3:], ct_tokens["source"][1][4:], msg="the moon") 
+        self.assertEqual(ct_tokens["target"][0][3:], ct_tokens["target"][1][4:], msg="the moon") 
+        self.assertEqual(ct_tokens["source"][1][0:4], ct_tokens["source"][2][1:5], msg="I am walking on")
+        self.assertEqual(ct_tokens["target"][1][0:4], ct_tokens["target"][2][1:5], msg="I am walking on")
 
     def test_same_words_have_same_ct_label_in_diff_datasets(self):
         [ct_tokens_train, ct_tokens_dev] = ct.create_sequence_labeling_dataset(
             self.three_label_data_skewed,
             dev_source=self.two_labels_dataset_balanced["source"],
         )
-        eye_am_walking_on = (
-            ct_tokens_train["source"][1][0:4] == ct_tokens_dev["source"][0][0:4]
-            and ct_tokens_train["target"][1][0:4] == ct_tokens_dev["target"][0][0:4]
-        )
-        assert eye_am_walking_on
+        self.assertEqual(ct_tokens_train["source"][1][0:4], ct_tokens_dev["source"][0][0:4], msg="I am walking on")
+        self.assertEqual(ct_tokens_train["target"][1][0:4], ct_tokens_dev["target"][0][0:4], msg="I am walking on")
 
     def test_num_ct_labels_exactly_matches_num_training_labels_two_labels(self):
-        """The number of control task labels is smaller or equal to the number of labels in the task training data"""
+        """The number of control task labels is equal to the number of labels in the task training data"""
         [ct_tokens] = ct.create_sequence_labeling_dataset(
             self.two_labels_dataset_balanced, sample_from="uniform"
         )
         labels_flat = [l for sublist in ct_tokens["target"] for l in sublist]
-        assert len(set(labels_flat)) == 2
+        self.assertEqual(len(set(labels_flat)), 2, msg="number of ct labels == number of labels in the task training data")
 
     def test_num_ct_labels_roughly_matches_num_training_labels_three_labels(self):
         """The number of control task labels is smaller or equal to the number of labels in the task training data"""
@@ -88,7 +80,7 @@ class TestCreateSeqLabelingControlTaskDatasets(unittest.TestCase):
             self.three_label_data_skewed, sample_from="same"
         )
         labels_flat = [l for sublist in ct_tokens["target"] for l in sublist]
-        assert len(set(labels_flat)) <= 3
+        self.assertLessEqual(len(set(labels_flat)), 3, msg="number of ct labels <= number of labels in the task training data")
 
     def test_num_ct_labels_roughly_matches_num_training_labels_four_labels(self):
         """The number of control task labels is smaller or equal to the number of labels in the task training data"""
@@ -96,7 +88,8 @@ class TestCreateSeqLabelingControlTaskDatasets(unittest.TestCase):
             self.small_dataset_four_labels, sample_from="same"
         )
         labels_flat = [l for sublist in ct_tokens["target"] for l in sublist]
-        assert len(set(labels_flat)) <= 4
+        self.assertLessEqual(len(set(labels_flat)), 4, msg="number of ct labels <= number of labels in the task training data")
+
 
     def test_uniform_distribution_matches(self):
         """Regardless of the label distribution in the training data, the control task labels are in a more or less uniform distribution"""
@@ -105,7 +98,7 @@ class TestCreateSeqLabelingControlTaskDatasets(unittest.TestCase):
         )
         labels_flat = [l for sublist in ct_tokens["target"] for l in sublist]
         label_freqs_dict = Counter(labels_flat)
-        assert max(label_freqs_dict.values()) < 600
+        self.assertLess(max(label_freqs_dict.values()), 600, msg="Regardless of the label distribution in the training data, the control task labels are in a more or less uniform distribution")
 
     def test_same_distribution_matches(self):
         """The label distribution in the control task data roughly matches the label distribution in the training data"""
@@ -114,4 +107,4 @@ class TestCreateSeqLabelingControlTaskDatasets(unittest.TestCase):
         )
         labels_flat = [l for sublist in ct_tokens["target"] for l in sublist]
         label_freqs_dict = Counter(labels_flat)
-        assert min(label_freqs_dict.values()) < 200
+        self.assertLess(min(label_freqs_dict.values()), 200, msg="label distribution in the ct data roughly matches the label distribution in the training data")
