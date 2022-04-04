c3e2734355623f595b5f1507e8d1c4fff31a655b
Hassan Sajjad <hassaan84s@gmail.com>
2022-04-04 13:36:58 +0300

    Add minimum threshold argument in `get_top_words` (#19)
    `get_top_words` hardcoded the threshold to deem a "word" to be relevant to a neuron. This commit makes the threshold an additional argument the user can specify when using the function.

--- neurox/analysis/corpus.py
+++ neurox/analysis/corpus.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 import numpy as np
 
 
-def get_top_words(tokens, activations, neuron, num_tokens=0):
+def get_top_words(tokens, activations, neuron, num_tokens=0, min_threshold=None):
     """
     Get top activating words for any given neuron.
 
@@ -32,7 +32,12 @@ def get_top_words(tokens, activations, neuron, num_tokens=0):
         Index of the neuron relative to ``X``
     num_tokens: int, optional
         Number of top tokens to return. Defaults to 0, which returns all tokens
-        with a non-neglible contribution to the variance
+        with a non-neglible contribution to the variance. Cannot be specified with
+        min_threshold
+    min_threshold: float, optional
+        Return top tokens that have a normalized score above the threshold. Ranges
+        from 0 to 1. Defaults to 0.1 which returns all tokens that have a score of
+        above 0.1. Cannot be specified with num_tokens
 
     Returns
     -------
@@ -40,7 +45,13 @@ def get_top_words(tokens, activations, neuron, num_tokens=0):
         List of tuples, where each tuple is a (token, score) element
 
     """
-    MIN_THRESHOLD = 0.1  # threshold for "negligible activation score"
+
+    if num_tokens != 0 and min_threshold is not None:
+        raise ValueError(
+            "Cannot specify both num_tokens and min_threshold at the same time"
+        )
+    elif min_threshold is None:
+        min_threshold = 0.1
 
     activation_values = [
         sentence_activations[:, neuron] for sentence_activations in activations
@@ -72,10 +83,10 @@ def get_top_words(tokens, activations, neuron, num_tokens=0):
     # Sort and filter scores
     sorted_types_scores = sorted(type_wise_scores, key=lambda x: -x[1])
     sorted_types_scores = [
-        (k, v) for (k, v) in sorted_types_scores if v > MIN_THRESHOLD
+        (k, v) for (k, v) in sorted_types_scores if v > min_threshold
     ]
 
     if num_tokens > 0:
-        sorted_types_scores = sorted_types_scores[:num_tokens]
+        sorted_types_scores = sorted_types_scores[:num_tokens] 
 
     return sorted_types_scores
