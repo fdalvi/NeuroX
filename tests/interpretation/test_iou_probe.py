7e6865c1b8dd032fe0aee905c816b87d6744ee91
fym0503 <fym0503@mail.ustc.edu.cn>
2022-08-07 20:25:57 +0800

    add comments

--- tests/interpretation/test_iou_probe.py
+++ tests/interpretation/test_iou_probe.py
@@ -11,13 +11,8 @@ class TestGetNeuronOrdering(unittest.TestCase):
     def test_get_neuron_ordering(self):
         "Basic get neuron ordering test"
 
-        # Create a weight matrix with 2 samples and 3 neurons
-        # Neuron 3 is the most important neuron as it has the highest weight in
-        # both samples, followed by Neuron 1 which has equal weight in sample 2
-        # but higher weight in sample 1, ending with Neuron 2
-        # mock_weight_matrix = [[4, 1, 5], [1, 1, 10]]
-        # expected_neuron_order = [2, 0, 1]
-        # probe_mock.parameters.return_value = [torch.Tensor(mock_weight_matrix)]
+        # The neuron 0 is perfectly correlated with label, the neuron 1 is partly correlated
+        # The neuron 2 is not correlated
 
         X = np.array([[-1, -1, 0], [-1, 0, 0], [1, 1, 0], [1, 0, 0]])
         y = np.array([0, 0, 1, 1])
